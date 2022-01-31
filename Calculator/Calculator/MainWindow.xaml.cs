8eca11cc53e11b6b30a7e69d2b12c22a6c79288e
Michał Ortmann <86731687+michalortmann@users.noreply.github.com>
2022-01-31 11:04:46 +0100

    Add files via upload

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double lastNumber, result;
+        SelectedOperator selectedOperator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lblResult.Content = "0";
+
+        }
+    };
+
+
